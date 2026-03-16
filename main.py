b65af572f928680c513a94d4173565abc142dce5
Meer Ghanee <meerghanee232@gmail.com>
2026-03-16 03:13:18 +0300

    improve sources linkedin indeed

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
-import json
-
 from agents.indeed_agent import search_indeed
+from agents.bayt_agent import search_bayt
 from utils.job_filter import is_relevant
 from utils.telegram_sender import send_job
 
 
-def load_seen_jobs():
-
-    with open("database/jobs_seen.json", "r") as f:
-        return json.load(f)
-
-
-def save_seen_jobs(seen):
-
-    with open("database/jobs_seen.json", "w") as f:
-        json.dump(seen, f)
-
-
 def run():
 
-    jobs = search_indeed()
+    jobs = []
 
-    seen_jobs = load_seen_jobs()
+    jobs.extend(search_indeed())
+    jobs.extend(search_bayt())
 
     print("Jobs Found:", len(jobs))
 
     for job in jobs:
-
-        print("Checking:", job["title"])
-
-        if job["link"] in seen_jobs:
-            continue
 
         if is_relevant(job["title"]):
 
@@ -44,10 +27,6 @@ Link: {job['link']}
 """
 
             send_job(message)
-
-            seen_jobs.append(job["link"])
-
-    save_seen_jobs(seen_jobs)
 
 
 run()
